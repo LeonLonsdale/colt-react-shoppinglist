12ec4d90b48e1c777155324b9dac780a8c3a03d6
LeonLonsdale <leon.dev@icloud.com>
2023-05-22 21:09:30 +0100

    basic shopping list component

---
+++ src/components/ShoppingList.jsx
@@ -0,0 +1,16 @@
+export default function ShoppingList({items}) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li
+          style={{
+            color: item.completed ? 'grey' : 'red',
+            textDecoration: item.completed ? 'line-through' : 'none',
+          }}
+        >
+          {item.item} - {item.quantity}
+        </li>
+      ))}
+    </ul>
+  );
+}
